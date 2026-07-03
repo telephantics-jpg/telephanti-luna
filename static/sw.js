3c51b95411ab098b9da10c1b2ae7fa2b065d2ec1
Telephantix <telephantix@gmail.com>
2026-07-03 10:06:12 -0400

    v142: mic replies speak back, fix voice complaints and empty abort bubbles

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "luna-avatar-v141";
+const CACHE = "luna-avatar-v142";
 const ASSETS = ["/static/avatars/brunette.glb", "/static/icons/icon-192.png"];
 
 self.addEventListener("install", (e) => {

8d84aa61bb48a8de86eca51a35b3616b3376b9f4
Telephantix <telephantix@gmail.com>
2026-07-08 18:39:37 -0400

    Fix blank camp: remove layout gate blocking draw, simplify size sync (build 236)

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ from firmament.paths import data_file, script_path
 
 STATS_PATH = data_file("luna_stats.json")
 PORT = int(os.getenv("PORT", os.getenv("LUNA_PORT", "8767")))
-LUNA_BUILD = "235-camp-calc-size"
+LUNA_BUILD = "236-camp-show-fix"
 
 log = logging.getLogger("luna")
 _lipsync_executor = ThreadPoolExecutor(max_workers=1)

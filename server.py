3b2e60263a1a600b708cd61fcd5f8c11b09930cb
Telephantix <telephantix@gmail.com>
2026-07-08 16:23:57 -0400

    Camp hotfix all3: text expand, Dionysus house, PC/Android/iOS

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ from firmament.paths import data_file, script_path
 
 STATS_PATH = data_file("luna_stats.json")
 PORT = int(os.getenv("PORT", os.getenv("LUNA_PORT", "8767")))
-LUNA_BUILD = "195-camp-luna-type"
+LUNA_BUILD = "206-camp-all3"
 
 log = logging.getLogger("luna")
 _lipsync_executor = ThreadPoolExecutor(max_workers=1)

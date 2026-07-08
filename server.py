4bd35beda0923d70d6260331d92764a6dfc5cf90
Telephantix <telephantix@gmail.com>
2026-07-07 20:44:42 -0400

    Faster mobile walk speed and subtle name badge

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ from firmament.paths import data_file, script_path
 
 STATS_PATH = data_file("luna_stats.json")
 PORT = int(os.getenv("PORT", os.getenv("LUNA_PORT", "8767")))
-LUNA_BUILD = "187-camp-odin-portrait"
+LUNA_BUILD = "188-camp-mobile-speed-name"
 
 log = logging.getLogger("luna")
 _lipsync_executor = ThreadPoolExecutor(max_workers=1)

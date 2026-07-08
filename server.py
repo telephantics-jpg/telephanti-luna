c5b315547e9465f1c94c8bd2e638cb0ff0021481
Telephantix <telephantix@gmail.com>
2026-07-08 16:57:34 -0400

    Revert camp UI to stable baseline, keep Ara/Mika, double-tap chat

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ from firmament.paths import data_file, script_path
 
 STATS_PATH = data_file("luna_stats.json")
 PORT = int(os.getenv("PORT", os.getenv("LUNA_PORT", "8767")))
-LUNA_BUILD = "210-camp-pc-fix"
+LUNA_BUILD = "211-camp-stable"
 
 log = logging.getLogger("luna")
 _lipsync_executor = ThreadPoolExecutor(max_workers=1)

cb4dd0819a66192b776e478038bf7a3713f18b44
Telephantix <telephantix@gmail.com>
2026-07-08 17:45:39 -0400

    Restore PC camp canvas/zoom from build 192 (0.38, stage rect sizing)

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ from firmament.paths import data_file, script_path
 
 STATS_PATH = data_file("luna_stats.json")
 PORT = int(os.getenv("PORT", os.getenv("LUNA_PORT", "8767")))
-LUNA_BUILD = "221-camp-pc-canvasfill"
+LUNA_BUILD = "222-camp-pc-192view"
 
 log = logging.getLogger("luna")
 _lipsync_executor = ThreadPoolExecutor(max_workers=1)

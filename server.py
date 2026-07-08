170bf901d293114889d122b45da321f101a4f246
Telephantix <telephantix@gmail.com>
2026-07-08 17:14:09 -0400

    Fix PC meadow spread and fit-to-frame layout

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ from firmament.paths import data_file, script_path
 
 STATS_PATH = data_file("luna_stats.json")
 PORT = int(os.getenv("PORT", os.getenv("LUNA_PORT", "8767")))
-LUNA_BUILD = "214-camp-meadow-chat"
+LUNA_BUILD = "215-camp-pc-meadow"
 
 log = logging.getLogger("luna")
 _lipsync_executor = ThreadPoolExecutor(max_workers=1)

cb858d88434c04ecbabb54a69f735b5808329e20
Telephantix <telephantix@gmail.com>
2026-07-08 19:08:18 -0400

    Fix mobile agent spread, talk selector, and heaven summon button

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ from firmament.paths import data_file, script_path
 
 STATS_PATH = data_file("luna_stats.json")
 PORT = int(os.getenv("PORT", os.getenv("LUNA_PORT", "8767")))
-LUNA_BUILD = "243-revert-242"
+LUNA_BUILD = "244-mobile-spread-ui"
 
 log = logging.getLogger("luna")
 _lipsync_executor = ThreadPoolExecutor(max_workers=1)

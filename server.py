3fdee9c38283d7113e4538f206a552851c39b5d0
Telephantix <telephantix@gmail.com>
2026-07-08 20:06:52 -0400

    Fix TX music button visibility on Android mobile

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ from firmament.paths import data_file, script_path
 
 STATS_PATH = data_file("luna_stats.json")
 PORT = int(os.getenv("PORT", os.getenv("LUNA_PORT", "8767")))
-LUNA_BUILD = "246-grok-spawn-tx"
+LUNA_BUILD = "247-android-tx-fab"
 
 log = logging.getLogger("luna")
 _lipsync_executor = ThreadPoolExecutor(max_workers=1)

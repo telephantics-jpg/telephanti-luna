385244f47645ee040f1dbdabd5d12198eec13749
Telephantix <telephantix@gmail.com>
2026-07-11 19:14:15 -0400

    Bump SW cache for logic dialogue build.

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE = "luna-avatar-v273-para-length";
+const CACHE = "luna-avatar-v274-logic-dialogue";
 const ASSETS = ["/static/avatars/brunette.glb", "/static/icons/icon-192.png"];
 
 self.addEventListener("install", (e) => {

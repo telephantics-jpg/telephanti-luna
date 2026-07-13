d39489c114348145ff517f90aeb749b863631125
Telephantix <telephantix@gmail.com>
2026-07-13 18:47:21 -0400

    Selectable speech bubbles brought to front with copyable panel.

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ from firmament.paths import data_file, script_path
 
 STATS_PATH = data_file("luna_stats.json")
 PORT = int(os.getenv("PORT", os.getenv("LUNA_PORT", "8767")))
-LUNA_BUILD = "289-EMOJI-PULSE"
+LUNA_BUILD = "290-SELECT-BUBBLE"
 
 log = logging.getLogger("luna")
 _lipsync_executor = ThreadPoolExecutor(max_workers=1)
--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "luna-avatar-v289-emoji-pulse"
+const CACHE = "luna-avatar-v290-select-bubble"
 const ASSETS = ["/static/avatars/brunette.glb", "/static/icons/icon-192.png"];
 
 self.addEventListener("install", (e) => {

700bd1f9a9c995f5b5501f972e47ba5e74513103
Telephantix <telephantix@gmail.com>
2026-08-15 13:01:46 -0400

    Camp talk: drop more director-prompt echoes from spoken lines.

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ from firmament.paths import data_file, script_path
 
 STATS_PATH = data_file("luna_stats.json")
 PORT = int(os.getenv("PORT", os.getenv("LUNA_PORT", "8767")))
-LUNA_BUILD = "326-MJOLNIR-SKY"
+LUNA_BUILD = "327-SPOKEN-ONLY"
 
 log = logging.getLogger("luna")
 _lipsync_executor = ThreadPoolExecutor(max_workers=1)

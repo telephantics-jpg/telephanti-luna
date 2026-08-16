cb8b94b7b730a909a1296159ead9602c89217a6f
Telephantix <telephantix@gmail.com>
2026-08-16 08:48:59 -0400

    2D camp: play Mjolnir lightning loop on every device.

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ from firmament.paths import data_file, script_path
 
 STATS_PATH = data_file("luna_stats.json")
 PORT = int(os.getenv("PORT", os.getenv("LUNA_PORT", "8767")))
-LUNA_BUILD = "330-GROK-ON"
+LUNA_BUILD = "331-MJOLNIR-2D"
 
 log = logging.getLogger("luna")
 _lipsync_executor = ThreadPoolExecutor(max_workers=1)

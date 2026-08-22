f16db073dadafd7fd0cfb1b82f2a7542998b3614
Telephantix <telephantix@gmail.com>
2026-08-22 15:37:38 -0400

    Thought bubbles stay on canvas and drag: grab the cloud over their head.

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ from firmament.paths import data_file, script_path
 
 STATS_PATH = data_file("luna_stats.json")
 PORT = int(os.getenv("PORT", os.getenv("LUNA_PORT", "8767")))
-LUNA_BUILD = "338-BUBBLE-DRAG"
+LUNA_BUILD = "339-THOUGHT-DRAG"
 
 log = logging.getLogger("luna")
 _lipsync_executor = ThreadPoolExecutor(max_workers=1)

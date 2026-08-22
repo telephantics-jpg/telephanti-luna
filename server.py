3c48904bf871648fa061d602bdc8f3c8b762f97a
Telephantix <telephantix@gmail.com>
2026-08-22 10:31:05 -0400

    PC camp UI: hide-all talk toggle that stays off, drag HUD chips out of the way.

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ from firmament.paths import data_file, script_path
 
 STATS_PATH = data_file("luna_stats.json")
 PORT = int(os.getenv("PORT", os.getenv("LUNA_PORT", "8767")))
-LUNA_BUILD = "333-CAMP-CHAT"
+LUNA_BUILD = "334-TALK-MIN"
 
 log = logging.getLogger("luna")
 _lipsync_executor = ThreadPoolExecutor(max_workers=1)
